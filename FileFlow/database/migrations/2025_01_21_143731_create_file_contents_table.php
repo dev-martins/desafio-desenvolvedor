b6dae388e0d3167d9bbc6d9ec34aeac6e4906f88
Claudio Martins <henrique_marty20@hotmail.com>
2025-01-22 14:57:17 -0300

    criando migrations

---
+++ FileFlow/database/migrations/2025_01_21_143731_create_file_contents_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('file_contents', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('upload_id')->constrained('uploads')->onDelete('cascade');
+            $table->date('RptDt')->nullable();
+            $table->string('TckrSymb')->nullable();
+            $table->string('MktNm')->nullable();
+            $table->string('SctyCtgyNm')->nullable();
+            $table->string('ISIN')->nullable();
+            $table->string('CrpnNm')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('file_contents');
+    }
+};
